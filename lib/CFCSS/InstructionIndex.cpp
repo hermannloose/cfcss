e2fa472891f3aac6b683a79a101e3555232403df
Hermann Loose <hermannloose@gmail.com>
2013-08-01 16:01:02 +0200

    Add InstructionIndex pass for easier access to call and return instructions.

---
+++ lib/CFCSS/InstructionIndex.cpp
@@ -0,0 +1,100 @@
+/**
+ * @author Hermann Loose <hermannloose@gmail.com>
+ *
+ * TODO(hermannloose): Add description.
+ */
+
+#define DEBUG_TYPE "cfcss"
+
+#include "InstructionIndex.h"
+
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+static const char *debugPrefix = "InstructionIndex: ";
+
+namespace cfcss {
+
+  InstructionIndex::InstructionIndex() : ModulePass(ID) {
+
+  }
+
+
+  void InstructionIndex::getAnalysisUsage(AnalysisUsage &AU) const {
+    AU.setPreservesAll();
+  }
+
+
+  bool InstructionIndex::runOnModule(Module &M) {
+    for (Module::iterator fi = M.begin(), fe = M.end(); fi != fe; ++fi) {
+      if (fi->isDeclaration()) {
+        continue;
+      }
+
+      CallList *callList = new CallList();
+      callsByFunction.insert(std::pair<Function*, CallList*>(fi, callList));
+
+      PrimaryCallMap *primaryCalls = new PrimaryCallMap();
+      primaryCallsByFunction.insert(std::pair<Function*, PrimaryCallMap*>(fi, primaryCalls));
+
+      ReturnList *returnList = new ReturnList();
+      returnsByFunction.insert(std::pair<Function*, ReturnList*>(fi, returnList));
+
+      for (Function::iterator bi = fi->begin(), be = fi->end(); bi != be; ++bi) {
+        for (BasicBlock::iterator ii = bi->begin(), ie = bi->end(); ii != ie; ++ii) {
+          if (CallInst *callInst = dyn_cast<CallInst>(ii)) {
+            Function *calledFunction = callInst->getCalledFunction();
+            if (!(calledFunction->isDeclaration() || calledFunction->isIntrinsic())) {
+              callList->push_back(callInst);
+
+              if (!primaryCalls->count(calledFunction)) {
+                primaryCalls->insert(std::pair<Function*, CallInst*>(calledFunction, callInst));
+              }
+            }
+          }
+
+          if (ReturnInst *returnInst = dyn_cast<ReturnInst>(ii)) {
+            returnList->push_back(returnInst);
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+
+  CallList* InstructionIndex::getCalls(Function * const F) {
+    return callsByFunction.lookup(F);
+  }
+
+
+  CallInst* InstructionIndex::getPrimaryCallTo(Function * const target,
+      Function * const container) {
+
+    PrimaryCallMap *primaryCalls = primaryCallsByFunction.lookup(container);
+
+    return primaryCalls->lookup(target);
+  }
+
+
+  ReturnList* InstructionIndex::getReturns(Function * const F) {
+    return returnsByFunction.lookup(F);
+  }
+
+
+  ReturnInst* InstructionIndex::getPrimaryReturn(Function * const F) {
+    ReturnList* returns = returnsByFunction.lookup(F);
+    assert(!returns->empty());
+
+    return returns->front();
+  }
+
+
+  char InstructionIndex::ID = 0;
+}
+
+static RegisterPass<cfcss::InstructionIndex> X("instruction-index", "Instruction Index (CFCSS)");
